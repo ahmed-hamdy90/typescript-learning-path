0cca97723f47d6f1ff66b151c41d9550fa4932fb
Ahmed Hamdy <ahamdy@idealratings.com>
2019-08-20 20:43:00 +0200

    Complete video 13 on Chapter 03 under Pluralsight Getting With Typescript

---
+++ code/getting started with typescript/Chapter-03/13/app/app.ts
@@ -0,0 +1,16 @@
+function startGame() {
+    // starting a new game
+
+    const playerName: string = "Andrey";
+    logPlayer(playerName);
+
+    var messagesElement = document.getElementById('messages');
+    messagesElement.innerText = 'Welcome to MultiMath! Starting new game';
+    console.log("Starting new game.");
+}
+
+function logPlayer(name) {
+    console.log(`New game starting for player: ${name}`);
+}
+
+document.getElementById('startGame').addEventListener('click', startGame);
