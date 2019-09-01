53cbc535bbb66b4f22a4e630a08eeac905f6e0fa
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2019-09-01 05:55:08 +0200

    I Complete all videos for Chapter 04 under Pluralsight Getting started with Typescript(pushing all code for demo videos under this Chapter)

---
+++ code/getting started with typescript/Chapter-04/24/app/app.ts
@@ -0,0 +1,30 @@
+function startGame() {
+    // starting a new game
+
+    let playerName: string | undefined = getInputValue('playername');
+
+    logPlayer(playerName);
+
+    postScore(80, playerName);
+}
+
+function logPlayer(name: string = "MultiMath Player"): void {
+    console.log(`New game starting for player: ${name}`);
+}
+
+function postScore(score: number, playerName: string = "MultiMath Player"): void {
+    let scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+}
+function getInputValue(elementID: string): string | undefined {
+
+    let inputElement: HTMLInputElement = <HTMLInputElement> document.getElementById(elementID);
+
+    if (inputElement.value === '') {
+        return undefined;
+    } else {
+        return inputElement.value;
+    }
+}
+
+document.getElementById('startGame')!.addEventListener('click', startGame);
---
+++ code/getting started with typescript/Chapter-04/26/js/app.js
@@ -0,0 +1,28 @@
+function startGame() {
+    // starting a new game
+    var playerName = getInputValue('playername');
+    logPlayer(playerName);
+    postScore(80, playerName);
+}
+function logPlayer(name) {
+    if (name === void 0) { name = "MultiMath Player"; }
+    console.log("New game starting for player: " + name);
+}
+function postScore(score, playerName) {
+    if (playerName === void 0) { playerName = "MultiMath Player"; }
+    var scoreElement = document.getElementById('postedScores');
+    scoreElement.innerText = score + " - " + playerName;
+}
+function getInputValue(elementID) {
+    var inputElement = document.getElementById(elementID);
+    if (inputElement.value === '') {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+document.getElementById('startGame').addEventListener('click', startGame);
+var logMessage = function (message) { return console.log(message); };
+logMessage('Welcome to MultiMath!');
+//# sourceMappingURL=app.js.map
